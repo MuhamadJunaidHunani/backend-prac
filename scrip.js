1cdc2c7ec86b4478f6257b91bff69b32e0bde92a
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2024-11-14 20:12:06 +0500

    add routes system

--- scrip.js
+++ scrip.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express()
 
+app.use((req , res , next)=>{
+    console.log('junaid');
+    next();
+})
 app.get('/', (req , res)=>{
-    res.send('hello world')
+    res.send('home')
+})
+app.get('/profile', (req , res)=>{
+    res.send('profile')
 })
 
-app.listen('3000');
+app.listen('4000');
